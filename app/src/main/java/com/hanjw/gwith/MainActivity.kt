63c8860546cd4a270cb229e8108cfc50f64b2c6e
HanJiWoong <hanjw8410@gmail.com>
2022-09-20 10:53:29 +0900

    Initial commit

---
+++ app/src/main/java/com/hanjw/gwith/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanjw.gwith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
